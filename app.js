bb680945057e0fa5660ab3383b068fce5fd05077
JokeFat <shindolifeshipudden@gmail.com>
2024-05-17 16:55:38 +0000

    d

--- app.js
+++ app.js
@@ -55,5 +55,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the frontend
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
